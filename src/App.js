7fde0bcff0094bc24af749cc87a10f8d26fd0fda
fujisawa <fujisawa5523@gmail.com>
2023-07-26 22:37:23 +0900

    Add individual data acquisition from API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { getAllPokemon } from "./utils/pokemon.js";
+import { getAllPokemon, getPokemon } from "./utils/pokemon.js";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       let res = await getAllPokemon(initialURL);
-      console.log(res);
+      loadPolemon(res.results);
       setLoading(false);
     };
     fetchPokemonData();
   }, []);
+
+  const loadPolemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecode = getPokemon(pokemon.url);
+        return pokemonRecode;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  console.log(pokemonData);
 
   return (
     <div className="App">
