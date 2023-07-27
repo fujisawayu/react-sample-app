73b8d7848fecdc0be036b480aa6d356df1268dad
fujisawa <fujisawa5523@gmail.com>
2023-07-27 22:15:20 +0900

    Add component for card display

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { getAllPokemon, getPokemon } from "./utils/pokemon.js";
+import Card from "./components/Card/Card";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
@@ -33,7 +34,11 @@ function App() {
       {loading ? (
         <h1>ロードしています・・・</h1>
       ) : (
-        <h1>データを取得しました</h1>
+        <div className="pokemonCardContainer">
+          {pokemonData.map((pokemon, i) => {
+            return <Card key={i} pokemon={pokemon} />;
+          })}
+        </div>
       )}
     </div>
   );
